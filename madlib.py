61dfd0e5ebdd50f7fa2c2cc921c119f7fd0e27ae
Apoorva Chaddha <54181480+ApChaddha@users.noreply.github.com>
2019-12-03 09:55:29 -0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+print('Hi there! welcome to MAD LIBS')
+noun = input('Enter what every student has? ')
+sol = input('Enter what we can find with internet? ')
+int = input('Enter what changed our lifes? ')
+way = input('And in which way? ')
+wit = input('Enter What is important to surf internet? ')
+
+print('Today, every student has ' + noun + '. We can find any ' + sol + ' with internet. ' + int
++  ' changed our lifes in a ' + way + ' way. All we need to surf Internet is a ' + wit)
